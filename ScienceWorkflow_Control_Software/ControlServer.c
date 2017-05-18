d99a953e07419df7bb998b14a8ab6d2aa57160cc
aaron <pabstaaron@gmail.com>
2017-05-18 14:24:38 -0600

    Wrote up command API and wrote server code for receiving and interpreting commands

--- ScienceWorkflow_Control_Software/ControlServer.c
+++ ScienceWorkflow_Control_Software/ControlServer.c
@@ -14,6 +14,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+//#include "more_string.h"
 
 #define BUFSIZE 1024
 #define PORTNO 8080  // Command listening port
@@ -21,7 +22,11 @@
 struct sockaddr_in server;
 char buf[BUFSIZE]; // Buffer for storing datagrams
 
-void handleCommand(char* cmd);
+void handleCommand(char* cmd, struct sockaddr_in client);
+void drive(unsigned short theta, unsigned short speed);
+void pan(unsigned short theta, unsigned short speed);
+void sense_list(struct sockaddr_in client);
+void sense_get(char id, struct sockaddr_in client);
 
 int main(int argc, char** argv){
   printf("Starting ScienceWorkflow Control Server\n");
@@ -69,18 +74,101 @@ int main(int argc, char** argv){
     hostaddrp = inet_ntoa(client.sin_addr);
     if (hostaddrp == NULL)
       error("ERROR: Could not parse client address");
-    
-    printf("server received datagram from %s (%s)\n", 
-	   hostp->h_name, hostaddrp);
-    printf("server received %d/%d bytes: %s\n", strlen(buf), n, buf);
 
-    handleCommand(buf);
+    handleCommand(buf, client);
   }
 }
 
 /*
- * Takes in a command and generates a reactionary control signal
+ * Splits a command string into parameters
  */
-void handleCommand(char* cmd){
+char** split_cmd(char* cmd, int* numCmds){
+  char** arr = calloc(3, sizeof(char*));
 
+  int i = 0;
+  char* pch = strtok(cmd, " ");
+  while(pch != NULL){
+    if(i > 2)
+      return arr;
+    
+    arr[i] = pch;
+    pch = strtok(NULL, " ");
+    i++;
+  }
+
+  *numCmds = i;
+  return arr;
+}
+
+// Converts directional arguements encoded in ASCII into their proper numerical formats
+unsigned short* parse_dir_cmds(char* theta, char* speed){
+  unsigned short stheta = theta[0];
+  stheta = stheta << 8;
+  stheta = stheta | theta[1];
+  
+  unsigned short sspeed = speed[0];
+  sspeed = sspeed << 8;
+  sspeed = sspeed | speed[1];
+
+  unsigned short* arr = malloc(2 * sizeof(unsigned short));
+  arr[0] = stheta;
+  arr[1] = sspeed;
+
+  return arr;
+}
+
+/*
+ * Takes in a command and responds accordingly.
+ */
+void handleCommand(char* cmd, struct sockaddr_in client){
+  // Split command about spaces
+  int* numargs = malloc(sizeof(int));
+  char** scmd = split_cmd(cmd, numargs);
+
+  // remove newline from command, if it is present
+  int clen = strlen(scmd[0]);
+  if(scmd[0][clen-1] == '\n')
+    scmd[0][clen-1] = 0;
+  
+  if(!strcmp(scmd[0], "DRIVE")){
+    if(*numargs != 3)
+      return;
+    short* args = parse_dir_cmds(scmd[1], scmd[2]);
+    drive(args[0], args[1]);
+    free(args);
+  }
+  else if(!strcmp(scmd[0], "PAN")){
+    if(*numargs != 3)
+      return;
+    short* args = parse_dir_cmds(scmd[1], scmd[2]);
+    pan(args[0], args[1]);
+    free(args);
+  }
+  else if(!strcmp(scmd[0], "SENSE_LIST")){
+    sense_list(client);
+  }
+  else if(!strcmp(scmd[0], "SENSE_GET")){
+    if(*numargs != 2)
+      return;
+    sense_get(scmd[1][0], client);
+  }
+
+  free(scmd);
+  free(numargs);
+}
+
+void drive(unsigned short theta, unsigned short speed){
+  printf("Received drive command with theta = %d and speed = %d\n", theta, speed);
+}
+
+void pan(unsigned short theta, unsigned short speed){
+  printf("Received pan command with theta = %d and speed = %d\n", theta, speed);
+}
+
+void sense_list(struct sockaddr_in client){
+  printf("Received sense_list command\n");
+}
+
+void sense_get(char id, struct sockaddr_in client){
+  printf("Received a sense get command for device %d\n", id);
 }
